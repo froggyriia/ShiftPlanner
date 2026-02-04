32cac9425bf143429f6017bb22d817e9c2c4789b
froggyriia <nira.osamu@yandex.ru>
2026-02-04 16:13:24 +0300

    Initial Commit

---
+++ Cozy Mess/Cozy_MessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cozy_MessApp.swift
+//  Cozy Mess
+//
+//  Created by Виктория on 04.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Cozy_MessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
